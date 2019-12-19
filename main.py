ab2fd53f312f28bdb74af35132c00ea6d4b445b3
ArchieGu <297353274@qq.com>
2019-12-19 09:45:29 +0800

    修改主函数，先进行省界判断再进行交点判断

--- main.py
+++ main.py
@@ -4,35 +4,43 @@ from Path.Map.utils import (
     load_specific_province,
     millerToXY,
 )
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 from Path.Map.utils import gen_cross_point, gen_line
 
-provices_point = load_province()
-'''
-point = (32770658,6998678)
-for prov, points in provices_point.items():
-    if is_in_province(provices_point, prov, point):
-        print('point in province: {}'.format(prov))
-'''
-# for prov,points in provices_point.items():
-#     plt.plot(points['lon'],points['lat'],'o-')
-# plt.show()
+def main():
 
-start = (33547259,7606963)
-end = (33121518, 6789409)
-x, y = gen_line(start, end)
-line = np.dstack((x, y))[0]
+    prov_inter_points = {}
+    point_start = (32770658,6998678)
+    point_end = (33121518, 6789409)
 
-for prov, points in provices_point.items():
-    lat = points.get('lat')
-    lon = points.get('lon')
-    lon, lat = millerToXY(lon, lat)
-    print('-----------------')
-    print(prov)
-    points = np.dstack((lon, lat))[0]
-    cross_point_list = gen_cross_point(line, points)
-    interaction_points = cross_point_list.bounds
-    if interaction_points:
-        print(interaction_points)
+    provices_point = load_province()
+    for prov, points in provices_point.items():
+        if is_in_province(provices_point, prov, point_start):
+            print('Start point is in province: {}'.format(prov))
+            start_prov = prov
+        if is_in_province(provices_point,prov,point_end):
+            print('End point is in province: {}'.format(prov))
+            end_prov = prov
+
+    x, y = gen_line(point_start, point_end)
+    line = np.dstack((x, y))[0]
+    for prov, points in provices_point.items():
+        lat = points.get('lat')
+        lon = points.get('lon')
+        lon, lat = millerToXY(lon, lat)
+        print('-----------------')
+        print(prov)
+        points = np.dstack((lon, lat))[0]
+        cross_point_list = gen_cross_point(line, points)
+        interaction_points = cross_point_list.bounds
+        if interaction_points:
+            prov_inter_points[prov] = interaction_points
+            print(interaction_points)
+    print(prov_inter_points)
+
+if __name__ == '__main__':
+    main()
+
